9c80930e7f01e7b5e312f34bf74302d0b2f726b2
chyl26 <chyl26@live.cn>
2017-05-21 08:11:09 +0800

    bitas基础框架

---
+++ REQM/Domain/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REQM.Domain
+{
+    /// <summary>
+    /// 用户实体类
+    /// </summary>
+    public class User
+    {
+        public string UserId { get; set; }
+
+        public string ClassifyId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string PassWord { get; set; }
+    }
+}
---
+++ REQM/Service/UserService.cs
@@ -0,0 +1,44 @@
+﻿using REQM.Domain;
+using REQM.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REQM.Service
+{
+    public class UserService
+    {
+        IRepository<User> repository = new MbRepository<User>();
+        /// <summary>
+        /// 新增用户
+        /// </summary>
+        /// <param name="user"></param>
+        public void Create(User user)
+        {
+            repository.Insert("InsertUser", user);
+        }
+        public void GetUserById(string userId)
+        {
+            User user = repository.GetByCondition("SelectUserByCondition", new User { UserId = userId });
+        }
+        public IList<User> GetUsers(int pageCount)
+        {
+            IList<User> userList = repository.GetList("SelectUserByCondition", new User { }, pageCount);
+            return userList;
+        }
+        public int GetCount()
+        {
+            int count = repository.GetObject<int>("SelectUserCount", null);
+            return count;
+        }
+        public void Update(User user)
+        {
+            repository.Update("UpdateUser", user);
+        }
+        public void Delete(string userId)
+        {
+            repository.Delete("DeleteUser", new User { UserId = userId });
+        }
+    }
+}
